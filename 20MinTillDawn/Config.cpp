db2287c077074459f6c578e28ccc4aebaa593036
ARTSALT <artur.guedes@alunos.ufersa.edu.br>
2025-07-24 20:30:32 -0300

    functional gun animations

--- 20MinTillDawn/Config.cpp
+++ 20MinTillDawn/Config.cpp
@@ -3,5 +3,6 @@
 float Config::stageTotalTime = 5.0f * 60; // 5 min
 
 float Config::shotCountdown = 0.300f;
+float Config::ReloadTime = 2.0f;
 float Config::shotDamage = 40.0;
 int Config::numMaxShots = 6;
--- 20MinTillDawn/Weapon.cpp
+++ 20MinTillDawn/Weapon.cpp
@@ -1,21 +1,26 @@
 #include "Weapon.h"
 #include "TileSet.h"
+#include "Config.h"
 
 bool flipped = false;
 
-Weapon::Weapon(Character* newCharacter, string newSprite, string newProjectileSprite) {
+Weapon::Weapon(Character* newCharacter, string newSprite) {
+	reloadTimer = new Timer();
+
 	TileSet* tileSet = new TileSet(newSprite, 32, 32, 5, 10);
 	anim = new Animation(tileSet, 0.1f, true);
 
-	uint rightReload[5] = { 0, 1, 2, 3, 4 };
-	uint leftReload[5] = { 5, 6, 7, 8, 9 };
+	uint rightGun[1] = { 0 };
+	uint leftGun[1] = { 5 };
+	uint rightReload[4] = { 1, 2, 3, 4 };
+	uint leftReload[4] = { 6, 7, 8, 9 };
 
-	anim->Add(RIGHTRELOAD, rightReload, 5);
-	anim->Add(LEFTRELOAD, leftReload, 5);
+	anim->Add(RIGHTGUN, rightGun, 1);
+	anim->Add(LEFTGUN, leftGun, 1);
+	anim->Add(RIGHTRELOAD, rightReload, 4);
+	anim->Add(LEFTRELOAD, leftReload, 4);
 
-	anim->Select(RIGHTRELOAD);
-
-	projectileSprite = newProjectileSprite;
+	anim->Select(RIGHTGUN);
 
 	character = newCharacter;
 
@@ -25,6 +30,8 @@ Weapon::Weapon(Character* newCharacter, string newSprite, string newProjectileSp
 	MoveTo(x, y);
 
 	direction = new Vector(0, 0);
+	reloading = false;
+	numShots = Config::numMaxShots;
 }
 
 Weapon::~Weapon()
@@ -33,11 +40,6 @@ Weapon::~Weapon()
 }
 
 void Weapon::Update() {
-	if (window->KeyDown(VK_LBUTTON))
-	{
-
-	}
-
 	//-------------------------------
 	//        Posicionamento
 	//-------------------------------
@@ -75,17 +77,49 @@ void Weapon::Update() {
 	if (rotation > 90 && rotation < 270 && !flipped)
 	{
         int frame = anim->Frame();
-        anim->Select(LEFTRELOAD);
-        anim->Frame(frame);
+
+		if (reloading)
+			anim->Frame(frame);
 
 		flipped = true;
 	}
 	else if ((rotation < 90 || rotation > 270) && flipped)
 	{
-		int frame = anim->Frame() - 5;
-		anim->Select(RIGHTRELOAD);
-		anim->Frame(frame);
+		int frame = anim->Frame() - 4;
+
+		if (reloading)
+			anim->Frame(frame);
 
 		flipped = false;
 	}
+
+	if (reloading)
+	{
+		anim->Select(flipped ? LEFTRELOAD : RIGHTRELOAD);
+	}
+	else {
+		anim->Select(flipped ? LEFTGUN : RIGHTGUN);
+	}
+
+	if (reloading)
+	{
+		anim->NextFrame();
+
+		if (reloadTimer->Elapsed() > Config::ReloadTime)
+		{
+			numShots = Config::numMaxShots;
+			reloadTimer->Reset();
+			reloadTimer->Stop();
+			reloading = false;
+		}
+	}
+}
+
+void Weapon::Reload() {
+	reloading = true;
+	reloadTimer->Start();
+}
+
+bool Weapon::Reloading() {
+	return reloading;
 }
